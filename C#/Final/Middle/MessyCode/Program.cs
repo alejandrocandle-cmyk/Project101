0622f0c5b6a53ea9ec1945ee9b0ea3895b80043a
alejandrocandle-cmyk <alejandrodab2@gmail.com>
2026-06-21 10:32:10 -0700

    Add files via upload

---
+++ C#/Final/Middle/MessyCode/Program.cs
@@ -0,0 +1,94 @@
+﻿/*
+/ Alejandro Garcia 
+/ Module10 practice A
+/ this code call on a static then check how much the book fee is then returns a result
+/ Test case:
+/ Dj, 250 ---> 50
+/ Band, 350 -> 52.5
+/ OpenMic, 350 --> 35
+/ Band, 500 ----> 75
+/ DJ, 200 ---> 50
+*/
+
+namespace MessyCode;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"The book fee: {CalculateBookingFee("DJ", 200)}");
+
+        static double CalculateBookingFee(string actType, int att)
+        {
+            double fee = 0;
+
+            if (actType == "DJ")
+            {
+                if (att > 200)
+                {
+                    fee = att * 0.20;
+                }
+                else
+                {
+                    if (att > 100)
+                    {
+                        fee = att * 0.25;
+                    }
+                    else
+                    {
+                        fee = att * 0.30;
+                    }
+
+                }
+            }
+            else if (actType == "Band")
+            {
+
+                if (att > 200)
+                {
+                    fee = att * 0.15;
+                }
+                else
+                {
+                    if (att > 100)
+                    {
+                        fee = att * 0.20;
+                    }
+                    else
+                    {
+                        fee = att * 0.28;
+                    }
+                }
+            }
+
+            else if (actType == "OpenMic")
+            {
+
+                if (att > 150)
+                {
+                    fee = att * 0.10;
+                }
+                else
+                {
+                    if (att > 50)
+                    {
+                        fee = att * 0.12;
+                    }
+                    else
+                    {
+                        fee = att * 0.15;
+                    }
+                }
+            }
+
+            else
+            {
+                fee = att * 0.50; // mystery acts are expensive!
+            }
+            return fee;
+
+        }
+    }
+}
+
+// this was a lot of if and else statment so I took some away to make it easy on the eyes and change the f to fee to better understand what it is
---
+++ C#/Final/Middle/RhythmRoom/Program.cs
@@ -0,0 +1,58 @@
+﻿/*
+/ Alejandro Garcia
+/ Module10 programming partice B
+/
+/
+/ iteration part 1 
+/ Console.WriteLine("write ten names");
+/ Console.ReadLine();
+/ Console.ReadLine();
+/ Console.ReadLine();
+/ Console.ReadLine();
+/ Console.ReadLine();
+/ Console.ReadLine();
+/ Console.ReadLine();
+/ Console.ReadLine();
+/ Console.ReadLine();
+/ Console.ReadLine();
+*/
+/*
+/ iteration part 2
+/ console.writeLine("How many people are coming?")
+/ people = console.ReadLine();
+/ for (int i = 0; i < people.length; i++)
+/ {
+/    console.ReadLine();
+/ }
+/
+*/
+
+Console.WriteLine("Write ten names");
+
+for (int i = 0; i < 10; i++)
+{
+    Console.ReadLine();
+}
+
+Console.WriteLine("How many people are coming?");
+string people = Console.ReadLine();
+int peopleint = Convert.ToInt32(people);
+
+int sum = -1;
+
+string[] part = new string[peopleint];
+Console.WriteLine("Put the people names");
+for(int i = 0; i < peopleint; i++)
+{
+    sum += 1;
+    Console.WriteLine($"Enter name {sum}");
+    part[sum] = Console.ReadLine();
+}
+
+int sumv2 = 0;
+Console.WriteLine("This is all the people that are coming");
+for (int i = 0; i < part.Length; i++)
+{
+    sumv2 += 1;
+    Console.WriteLine($"{sumv2}: {part[i]}");
+}
